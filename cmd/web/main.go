1c95fedc30a12574431d17799f3729cdddd93f11
Sam Maton <sam.maton91@gmail.com>
2025-02-02 21:51:29 +0000

    setup other routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -15,6 +15,10 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
 		t, _ := template.New("home").ParseFiles("ui/html/base.html", "ui/html/partials/nav.html", "ui/html/pages/index.html")
 		err := t.ExecuteTemplate(w, "base", nil)
 		if err != nil {
@@ -28,6 +32,18 @@ func main() {
 		if err != nil {
 			log.Println(err)
 		}
+	})
+
+	mux.HandleFunc("/protected", func(w http.ResponseWriter, r *http.Request) {
+		t, _ := template.New("login").ParseFiles("ui/html/base.html", "ui/html/partials/nav.html", "ui/html/pages/protected.html")
+		err := t.ExecuteTemplate(w, "base", nil)
+		if err != nil {
+			log.Println(err)
+		}
+	})
+
+	mux.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/", http.StatusOK)
 	})
 
 	srv := &http.Server{
